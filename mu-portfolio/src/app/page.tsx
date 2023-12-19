24c4a7c0743efc06900d22e8e57f505ca32674b4
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-12-19 00:37:42 -0500

    added hero area component

--- mu-portfolio/src/app/page.tsx
+++ mu-portfolio/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import HeroArea from './components/hero_area'
 
 export default function Home() {
   return (
     <main className="w-screen h-screen">
-
+      <HeroArea />
     </main>
   )
 }
