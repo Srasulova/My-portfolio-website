469687715a5ef09b8beff3a46c11d92b18918edc
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-12-20 15:11:35 -0500

    changed text color

--- mu-portfolio/src/app/components/about_me.tsx
+++ mu-portfolio/src/app/components/about_me.tsx
@@ -12,16 +12,16 @@ export default function AboutMe() {
                         </div>
                         <div className="w-full lg:max-w-xl lg:flex-auto">
 
-                            <h2 className="text-3xl mb-12 font-bold tracking-wide text-gray-900 sm:text-4xl">About me</h2>
-                            <p className="mb-4">Born and raised in the picturesque landscapes of Azerbaijan, I&apos;m Sabina, a proud Azerbaijani soul now navigating the American adventure with my greatest joy — my family and a charming baby girl. </p>
+                            <h2 className="text-3xl mb-12 font-bold tracking-wide text-rose-500 sm:text-4xl">About me</h2>
+                            <p className="mb-4 text-zinc-700">Born and raised in the picturesque landscapes of Azerbaijan, I&apos;m Sabina, a proud Azerbaijani soul now navigating the American adventure with my greatest joy — my family and a charming baby girl. </p>
 
-                            <p className="mb-4">For more than 15 years, I waltzed through the realms of customer service, but the thrill started to fade. Seeking a new chapter, I took a leap into the unknown and found my passion in web development. A fresh face in the web development world, I&apos;m eagerly embracing the journey, building websites, applications, and anything that involves code. Still got a long road ahead, and I must admit, it&apos;s one exciting learning curve.</p>
+                            <p className="mb-4  text-zinc-700">For more than 15 years, I waltzed through the realms of customer service, but the thrill started to fade. Seeking a new chapter, I took a leap into the unknown and found my passion in web development. A fresh face in the web development world, I&apos;m eagerly embracing the journey, building websites, applications, and anything that involves code. Still got a long road ahead, and I must admit, it&apos;s one exciting learning curve.</p>
 
-                            <p className="mb-4">I thrive in collaborative spaces, where creativity is the name of the game, and challenges are my playground. Being naturally curious, I&apos;m that person who loves diving into new things, be it coding complexities or the pages of a good book.</p>
+                            <p className="mb-4  text-zinc-700">I thrive in collaborative spaces, where creativity is the name of the game, and challenges are my playground. Being naturally curious, I&apos;m that person who loves diving into new things, be it coding complexities or the pages of a good book.</p>
 
-                            <p className="mb-4">In addition to coding adventures, I&apos;m all about helping others and soaking up family time. Whether it&apos;s cracking a coding problem, turning the mundane into something exciting, or connecting with fellow enthusiasts to chat about career growth, technologies, and life in general, count me in!</p>
+                            <p className="mb-4  text-zinc-700">In addition to coding adventures, I&apos;m all about helping others and soaking up family time. Whether it&apos;s cracking a coding problem, turning the mundane into something exciting, or connecting with fellow enthusiasts to chat about career growth, technologies, and life in general, count me in!</p>
 
-                            <p className="mb-4">My portfolio isn&apos;t just about showcasing projects; it&apos;s a visual story of my growth, resilience, and the joy of constantly evolving. Join me on this adventure, where every line of code is a step closer to creating something extraordinary. Cheers to the journey!</p>
+                            <p className="mb-4  text-zinc-700">My portfolio isn&apos;t just about showcasing projects; it&apos;s a visual story of my growth, resilience, and the joy of constantly evolving. Join me on this adventure, where every line of code is a step closer to creating something extraordinary. Cheers to the journey!</p>
                         </div>
                     </div>
                 </div>
