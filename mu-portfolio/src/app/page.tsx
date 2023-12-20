f6e4b00098c2a547a1168582baeab569bf134ce8
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-12-20 15:12:34 -0500

    added my tech stack component to page

--- mu-portfolio/src/app/page.tsx
+++ mu-portfolio/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import HeroArea from './components/hero_area'
 import AboutMe from './components/about_me'
+import MyTechStack from './components/my_tech_stack'
 
 export default function Home() {
   return (
     <main className="w-screen h-screen">
       <HeroArea />
       <AboutMe />
+      <MyTechStack />
     </main>
   )
 }
