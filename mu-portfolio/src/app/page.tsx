01b595eb88c0e8f65b345969bbcff0f5c824bf43
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-12-20 11:27:43 -0500

    added about me to the page

--- mu-portfolio/src/app/page.tsx
+++ mu-portfolio/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import HeroArea from './components/hero_area'
+import AboutMe from './components/about_me'
 
 export default function Home() {
   return (
     <main className="w-screen h-screen">
       <HeroArea />
+      <AboutMe />
     </main>
   )
 }
