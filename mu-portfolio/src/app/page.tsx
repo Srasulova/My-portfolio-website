9ab354ee3ed431ec43b2bccd57c01bdb65f19fd5
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-12-21 11:16:42 -0500

    added talk is cheap pic

--- mu-portfolio/src/app/page.tsx
+++ mu-portfolio/src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import HeroArea from './components/hero_area'
 import AboutMe from './components/about_me'
 import MyTechStack from './components/my_tech_stack'
+import MyProjects from './components/my_projects'
+import talkIsCheap from "../../public/talkischeap.jpg"
 
 export default function Home() {
   return (
@@ -9,6 +11,13 @@ export default function Home() {
       <HeroArea />
       <AboutMe />
       <MyTechStack />
+      <div className="w-screen h-1/2 flex flex-col">
+        <Image src={talkIsCheap} alt="" className=" w-full h-full relative isolate object-cover object-bottom " />
+        <blockquote className=' flex flex-col'> <p>“Talk is cheap. Show me the code.”</p>
+          <p>Linus Torvalds</p>
+        </blockquote>
+      </div>
+      <MyProjects />
     </main>
   )
 }
