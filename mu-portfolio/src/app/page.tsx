c659301d93106b8b232838f014cc9b727f743406
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-12-22 12:43:55 -0500

    added talkIsCheap, contacts and footer

--- mu-portfolio/src/app/page.tsx
+++ mu-portfolio/src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client"
 import Image from 'next/image'
 import HeroArea from './components/hero_area'
 import AboutMe from './components/about_me'
 import MyTechStack from './components/my_tech_stack'
 import MyProjects from './components/my_projects'
 import talkIsCheap from "../../public/talkischeap.jpg"
+import Contacts from './components/contacts'
+import Footer from './components/footer'
 
 export default function Home() {
   return (
@@ -11,13 +14,18 @@ export default function Home() {
       <HeroArea />
       <AboutMe />
       <MyTechStack />
-      <div className="w-screen h-1/2 flex flex-col">
-        <Image src={talkIsCheap} alt="" className=" w-full h-full relative isolate object-cover object-bottom " />
-        <blockquote className=' flex flex-col'> <p>“Talk is cheap. Show me the code.”</p>
-          <p>Linus Torvalds</p>
-        </blockquote>
+      <div className='w-screen h-3/5 '>
+        <div className="relative isolate overflow-hidden">
+          <Image src={talkIsCheap} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover object-bottom blur-sm" />
+          <div className="mx-auto max-w-4xl py-32 sm:py-48 lg:py-56 text-center">
+            <p className="text-4xl font-semibold tracking-wider text-white sm:text-4xl">“Talk is cheap. Show me the code.” </p>
+            <p className="mt-6 text-lg leading-8 text-gray-100 tracking-wider italic"> - Linus Torvalds</p>
+          </div>
+        </div>
       </div>
       <MyProjects />
+      <Contacts />
+      <Footer />
     </main>
   )
 }
